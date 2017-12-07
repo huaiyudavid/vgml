04e7a4dfc0f0acdcf0666ef71a9ee4778ab74209
Ubuntu <ubuntu@ip-172-31-44-178.ec2.internal>
2017-12-07 03:15:19 +0000

    change master location

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-~/spark-2.2.0-bin-hadoop2.7/bin/spark-submit --master spark://169.254.206.2:7077 --executor-memory 2g server.py
+~/spark-2.2.0-bin-hadoop2.7/bin/spark-submit --master spark://ip-172-31-44-178.ec2.internal:7077 --executor-memory 2g server.py
