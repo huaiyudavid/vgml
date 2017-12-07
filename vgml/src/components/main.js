60b201ae7a1e0725dc91e2039b3b7c5f6dd8e81f
churkle <charfei70@gmail.com>
2017-12-07 03:24:31 -0600

    Added a bunch of code

--- vgml/src/components/main.js
+++ vgml/src/components/main.js
@@ -1,27 +1,49 @@
 'use strict';
 
 import React from 'react';
-import { Typeahead } from 'react-typeahead';
+import { Typeahead } from 'react-bootstrap-typeahead';
 import GAMES from '../games.js';
 
 let optionSelected = function(result) {
     console.log(GAMES[result]);
 }
 
-const Main = () => <div>
-        <div className="row">
-            <h2>VGML</h2>
-        </div>
-        <div className="row typeahead">
-            <Typeahead
-                displayOption="game"
-                filterOption="game"
-                options={GAMES}
-                maxVisible={10}
-                onOptionSelected={optionSelected}
-            />
-        </div>
-        
-    </div>;
+class Main extends React.Component {
+    state = {
+        ratings: []
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <h2>VGML</h2>
+                </div>
+                <div className="typeahead">
+                        <Typeahead
+                            multiple
+                            onChange={selected => {
+                                this.state.ratings = selected.map(obj => { return { id: obj.id, rating: 200 }});
+                            }}
+                            labelKey={'game'}
+                            options={GAMES}
+                            maxResults={10}
+                        />
+                        <button className="btn-secondary" onClick={() => {
+                            console.log(this.state);
+                            fetch("http://54.174.204.120:5432/ratings", {
+                                method: "POST",
+                                body: JSON.stringify(this.state)
+                            }).then(response => {
+                                console.log(response);
+                            });
+                        }}>
+                            Submit
+                        </button>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Main;
